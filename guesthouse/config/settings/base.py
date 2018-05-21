6e4cd7a46ad07c57b0ba31295481fdf038381fd3
Willem Drescher <willemdrescher@yahoo.com>
2018-05-21 12:18:10 -0400

    start management app

--- guesthouse/config/settings/base.py
+++ guesthouse/config/settings/base.py
@@ -82,6 +82,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'guesthouse.users.apps.UsersConfig',
+    'guesthouse.management'
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
