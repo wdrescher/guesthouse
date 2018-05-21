5c43c3dd881a97e3528aa33948393a2249aad34d
Willem Drescher <willemdrescher@yahoo.com>
2018-05-21 13:26:05 -0400

    models

--- guesthouse/guesthouse/management/admin.py
+++ guesthouse/guesthouse/management/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Product, Project, Task, Resource, Comment
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Resource)
+admin.site.register(Comment)
--- guesthouse/guesthouse/management/models.py
+++ guesthouse/guesthouse/management/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from guesthouse.users import models as user_model
 # Create your models here.
+MAX_LENGTH = 200
+LONG_LENGTH = 500
+
+class Product(models.Model):
+    cost = models.DecimalField(decimal_places=2, max_digits=20)
+    title = models.TextField(max_length=MAX_LENGTH)
+    description = models.TextField(max_length=LONG_LENGTH)
+    worker = models.ManyToManyField(user_model.User)
+
+class Project(models.Model):
+    title = models.TextField(max_length=MAX_LENGTH)
+    product = models.ForeignKey(Product, on_delete="CASCADE")
+
+class Task(models.Model):
+    title = models.TextField(max_length=MAX_LENGTH)
+    description = models.TextField(max_length=LONG_LENGTH)
+    cost = models.DecimalField(decimal_places=2, max_digits=20)
+    due_date = models.DateField()
+    project = models.ForeignKey(Project, on_delete="CASCADE")
+
+class Resource(models.Model):
+    name = models.TextField(max_length=MAX_LENGTH)
+    description = models.TextField(max_length=LONG_LENGTH)
+    owner = models.ForeignKey(user_model.User, on_delete="CASCADE")
+
+class Comment(models.Model):
+    body = models.TextField(max_length=1000)
+    author = models.ForeignKey(user_model.User, on_delete="CASCADE")
+    project = models.ForeignKey(Project, on_delete="CASCADE")
